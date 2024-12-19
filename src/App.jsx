0005bffb1d947ba13c4eb43adc3a21bc23398bdd
YRExecutive <operations@yrexecutive.co.uk>
2024-12-19 16:10:28 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+
+function App() {
+  return (
+    <div className='w-full h-full bg-zinc-800'>
+     <Navbar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav className='flex justify-between items-center py-6 px-8 md:px-32 bg-white drop-shadow-md'>
+        Navbar
+    </nav>
+  )
+}
+
+export default Navbar
+  
